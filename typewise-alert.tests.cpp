7175d04d5859f49b16236ed696d14a4fffd04c8a
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-08-20 19:58:19 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,73 +1,53 @@
-#include "typewise-alert.h"
 #include <gtest/gtest.h>
-#include <sstream>
+#include "typewise-alert.h"
 
-// Helper function to capture printed output
-std::string captureOutput(void (*func)(TypewiseAlert::BreachType), TypewiseAlert::BreachType breachType) {
-    std::ostringstream oss;
-    std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
-    std::cout.rdbuf(oss.rdbuf());
-
-    func(breachType);
-
-    std::cout.rdbuf(oldCoutStreamBuf);
-    return oss.str();
+// Test to check the inferBreach function with different scenarios
+TEST(TypewiseAlertTest, InferBreach) {
+    EXPECT_EQ(TypewiseAlert::inferBreach(25, 20, 30), TypewiseAlert::BreachType::NORMAL);
+    EXPECT_EQ(TypewiseAlert::inferBreach(15, 20, 30), TypewiseAlert::BreachType::TOO_LOW);
+    EXPECT_EQ(TypewiseAlert::inferBreach(35, 20, 30), TypewiseAlert::BreachType::TOO_HIGH);
 }
 
-// Test classifyTemperatureBreach function
+// Test to verify the classifyTemperatureBreach function for different cooling types
 TEST(TypewiseAlertTest, ClassifyTemperatureBreach) {
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 34.0), TypewiseAlert::BreachType::NORMAL);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 36.0), TypewiseAlert::BreachType::TOO_HIGH);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, -1.0), TypewiseAlert::BreachType::TOO_LOW);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 44.0), TypewiseAlert::BreachType::NORMAL);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 46.0), TypewiseAlert::BreachType::TOO_HIGH);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, -1.0), TypewiseAlert::BreachType::TOO_LOW);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, 39.0), TypewiseAlert::BreachType::NORMAL);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, 41.0), TypewiseAlert::BreachType::TOO_HIGH);
-    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, -1.0), TypewiseAlert::BreachType::TOO_LOW);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 30), TypewiseAlert::BreachType::NORMAL);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 40), TypewiseAlert::BreachType::TOO_HIGH);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 50), TypewiseAlert::BreachType::TOO_HIGH);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, -5), TypewiseAlert::BreachType::TOO_LOW);
 }
 
-// Test sendToController function
-TEST(TypewiseAlertTest, SendToController) {
-    std::string output = captureOutput([](TypewiseAlert::BreachType breachType) {
-        TypewiseAlert::sendToController(breachType);
-    }, TypewiseAlert::BreachType::TOO_HIGH);
-    EXPECT_TRUE(output.find("feed") != std::string::npos);
-    EXPECT_TRUE(output.find("1") != std::string::npos);
+// Mocking a test for sending alerts to the controller
+TEST(TypewiseAlertTest, CheckAndAlertToController) {
+    // Redirect stdout to a stringstream to check output
+    testing::internal::CaptureStdout();
+    
+    TypewiseAlert::BatteryCharacter batteryChar = { TypewiseAlert::CoolingType::PASSIVE_COOLING, "BrandA" };
+    TypewiseAlert::checkAndAlert(TypewiseAlert::AlertTarget::TO_CONTROLLER, batteryChar, 50);
+    
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "feed : 2\n");  // TOO_HIGH is expected to be 2
 }
 
-// Test sendToEmail function
-TEST(TypewiseAlertTest, SendToEmail) {
-    std::string output = captureOutput([](TypewiseAlert::BreachType breachType) {
-        TypewiseAlert::sendToEmail(breachType);
-    }, TypewiseAlert::BreachType::TOO_HIGH);
-    EXPECT_TRUE(output.find("To: a.b@c.com") != std::string::npos);
-    EXPECT_TRUE(output.find("Hi, the temperature is too high") != std::string::npos);
-
-    output = captureOutput([](TypewiseAlert::BreachType breachType) {
-        TypewiseAlert::sendToEmail(breachType);
-    }, TypewiseAlert::BreachType::NORMAL);
-    EXPECT_TRUE(output.empty());
+// Mocking a test for sending alerts to email
+TEST(TypewiseAlertTest, CheckAndAlertToEmail) {
+    // Redirect stdout to a stringstream to check output
+    testing::internal::CaptureStdout();
+    
+    TypewiseAlert::BatteryCharacter batteryChar = { TypewiseAlert::CoolingType::PASSIVE_COOLING, "BrandA" };
+    TypewiseAlert::checkAndAlert(TypewiseAlert::AlertTarget::TO_EMAIL, batteryChar, 50);
+    
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
 }
 
-// Test checkAndAlert function
-TEST(TypewiseAlertTest, CheckAndAlert) {
-    TypewiseAlert::BatteryCharacter batteryChar = { TypewiseAlert::CoolingType::PASSIVE_COOLING, "BrandX" };
-
-    std::string emailOutput = captureOutput([](TypewiseAlert::BreachType breachType) {
-        TypewiseAlert::sendToEmail(breachType);
-    }, TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 36.0));
-    EXPECT_TRUE(emailOutput.find("To: a.b@c.com") != std::string::npos);
-    EXPECT_TRUE(emailOutput.find("Hi, the temperature is too high") != std::string::npos);
-
-    std::string controllerOutput = captureOutput([](TypewiseAlert::BreachType breachType) {
-        TypewiseAlert::sendToController(breachType);
-    }, TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 36.0));
-    EXPECT_TRUE(controllerOutput.find("feed") != std::string::npos);
-    EXPECT_TRUE(controllerOutput.find("1") != std::string::npos);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+// Test to ensure normal temperatures do not trigger alerts
+TEST(TypewiseAlertTest, NoAlertForNormalTemperature) {
+    // Redirect stdout to a stringstream to check output
+    testing::internal::CaptureStdout();
+    
+    TypewiseAlert::BatteryCharacter batteryChar = { TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, "BrandB" };
+    TypewiseAlert::checkAndAlert(TypewiseAlert::AlertTarget::TO_EMAIL, batteryChar, 40);
+    
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "");  // Expect no output since temperature is normal
 }
