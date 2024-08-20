a32f30d95cba42866d60cf3297f2e29f4d324403
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-08-20 20:55:26 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,6 +1,17 @@
 #include "typewise-alert.h"
 #include <gtest/gtest.h>
 
+TEST(TypewiseAlertTest, ClassifyTemperatureBreach) {
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 25), TypewiseAlert::BreachType::NORMAL);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 15), TypewiseAlert::BreachType::TOO_LOW);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::PASSIVE_COOLING, 35), TypewiseAlert::BreachType::TOO_HIGH);
+
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 40), TypewiseAlert::BreachType::NORMAL);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 50), TypewiseAlert::BreachType::TOO_HIGH);
+
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, 20), TypewiseAlert::BreachType::NORMAL);
+    EXPECT_EQ(TypewiseAlert::classifyTemperatureBreach(TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, -5), TypewiseAlert::BreachType::TOO_LOW);
+}
 // Mocking a test for sending alerts to the controller
 TEST(TypewiseAlertTest, CheckAndAlertToController) {
     // Redirect stdout to a stringstream to check output
