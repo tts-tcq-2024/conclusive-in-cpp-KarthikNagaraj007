da7fe4173e437b43c3e5acd409c7bfe95c84c495
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-08-20 19:51:42 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -27,6 +27,7 @@ public:
 
     static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
     static void checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
+friend class TypewiseAlertTest;
 
 private:
     struct CoolingLimits {
